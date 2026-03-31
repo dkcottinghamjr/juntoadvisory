788e1db5195db1e000f9fdf27a53ed091519ba31
Daniel Cottingham <danielc@Daniels-MacBook-Pro.local>
2026-03-30 21:57:08 -0400

    Remove basePath, add custom domain

--- next.config.mjs
+++ next.config.mjs
@@ -1,8 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   output: 'export',
-  basePath: '/juntoadvisory',
-  assetPrefix: '/juntoadvisory/',
   images: {
     unoptimized: true,
   },
